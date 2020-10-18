903207df3c6d103ee353661e3dcd2b4001773482
Conni <connithekiwi@gmail.com>
2020-10-18 01:20:28 +0000

    Update API - Correct listing of predefined user struct

--- Api/TransVibe_Webserver.js
+++ Api/TransVibe_Webserver.js
@@ -1,8 +1,26 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const querystring = require('querystring');
+
 const http = require('http');
 const url = require('url');
 const port = 25565;
 
 const fs = require('fs');
+
+const client = new Discord.Client();
+
+// Bot status
+client
+  .on("connected", () => {
+    console.warn("Fyre has reconnected!");
+  })
+  .on("reconnecting", () => {
+    console.warn("Fyre is reconnecting...");
+  })
+  .on("disconnect", () => {
+    console.warn("Warning! Fyre has disconnected!");
+  });
 
 http.createServer((req, res) => {
 	
@@ -12,17 +30,19 @@ http.createServer((req, res) => {
 	
 	if(pathN.startsWith("/Api/getRecentJoin"))
 		{
-			const params = current_url.searchParams;
+			var queryStr = urlObj.search.substring(1);
+			var params = querystring.parse(queryStr);
+			console.log(params);
 			
-			const interation = search_params.get('interation');
-			const type = search_params.get('type');
+			const interationS = params.interation;
 
 			res.setHeader('Content-Type', 'application/json');
     		res.end(
 			
 			JSON.stringify(
 				{ 
-					Username: "Conni!~#0920", 
+					interation: interationS,
+					userName: "Conni!~#0920", 
 					id: 123456789, 
 					timeSinceJoin: "A long time!"
 				} 
