f0069c41c6bc3a4561b769296ae672c14665293b
Conni <connithekiwi@gmail.com>
2020-10-18 16:03:18 +0000

    Discord OAuth

--- Api/TransVibe_Webserver.js
+++ Api/TransVibe_Webserver.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
 const querystring = require('querystring');
+const fetch = require('node-fetch');
 
 const http = require('http');
 const url = require('url');
@@ -22,14 +23,69 @@ client
     console.warn("Warning! Fyre has disconnected!");
   });
 
+client.on("ready", async () => {   
+	console.log("CLIENT");
+	console.log(client);
+});
+
 http.createServer((req, res) => {
 	
-	const urlObj = url.parse(req.url, true);
+		const urlObj = url.parse(req.url, true);
+
+	
+	
+		if (urlObj.query.code) {
+		const accessCode = urlObj.query.code;
+		const data = {
+			client_id: '767409581458194453',
+			client_secret: 'fp4yxDabFu3zznhHPU2C80jtqQweZu3X',
+			grant_type: 'authorization_code',
+			redirect_uri: 'http://transvibe.club:25565/',
+			code: accessCode,
+			scope: 'the scopes',
+		};
+
+		fetch('https://discord.com/api/oauth2/token', {
+			method: 'POST',
+			body: new URLSearchParams(data),
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+		})
+			.then(discordRes => discordRes.json())
+			.then(info => {
+				console.log(info);
+				return info;
+			})
+			.then(info => fetch('https://discord.com/api/users/@me', {
+				headers: {
+					authorization: `${info.token_type} ${info.access_token}`,
+				},
+			}))
+			.then(userRes => userRes.json())
+			.then(console.log);
+	}
+
+	if (urlObj.pathname === '/') {
+		responseCode = 200;
+		content = fs.readFileSync('../index.html');
+						res.writeHead(200, {
+					'content-type': 'text/html;charset=utf-8',
+				});
+
+				res.write(content);
+				res.end();
+		return;
+	}
+
+	
 
 	pathN = urlObj.pathname;
 	
 	if(pathN.startsWith("/Api/getRecentJoin"))
 		{
+			console.log(client.guilds.cache.array());
+			
 			var queryStr = urlObj.search.substring(1);
 			var params = querystring.parse(queryStr);
 			console.log(params);
@@ -50,24 +106,28 @@ http.createServer((req, res) => {
 			return;
 		}
 
-	// read file sample.html
-	fs.readFile('../Ari_Test.html',
-		// callback function that is called when reading file is done
-		function(err, data) { 
-			if (err) throw err;
-			// data is a buffer containing file content
-			let content = data.toString('utf8');
-		
-			console.log("Request made for!");
-			console.log(pathN);
+	if(pathN.startsWith("/admin"))
+	{
+		// read file sample.html
+		fs.readFile('../Ari_Test.html',
+			// callback function that is called when reading file is done
+			function(err, data) { 
+				if (err) throw err;
+				// data is a buffer containing file content
+				let content = data.toString('utf8');
 
-			res.writeHead(200, {
-				'content-type': 'text/html;charset=utf-8',
-			});
+				console.log("Request made for!");
+				console.log(pathN);
 
-			res.write(content);
-			res.end();
+				res.writeHead(200, {
+					'content-type': 'text/html;charset=utf-8',
+				});
 
-	});
+				res.write(content);
+				res.end();
+
+		});		
+	}
+
 })
 .listen(port);
